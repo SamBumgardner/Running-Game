9b44e51a61aea7d3c98b0b46a0060eed7ba19a86
SamBumgardner <sambumgardner1@gmail.com>
2020-01-18 13:25:39 -0600

    basic-platforming - refactored for code style.
    Going to generally match how I code at work, to save my muscle memory
    some effort.

--- source/state/PlayState.hx
+++ source/state/PlayState.hx
@@ -1,16 +1,14 @@
-package;
+package state;
 
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
-	override public function create():Void
-	{
+	override public function create():Void {
 		super.create();
 	}
 
-	override public function update(elapsed:Float):Void
-	{
+	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
 	}
 }
