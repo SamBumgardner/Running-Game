feae932cfc395c5ec49d9aad677cbea35af6aeed
SamBumgardner <sambumgardner1@gmail.com>
2020-01-22 18:25:36 -0600

    auto-run - implemented auto player movement.
    The current implementation isn't all that clean code-wise. We're
    layering on complexity within the Player class in a way that's going to
    make future changes progressively more difficult and risky. That will be
    fixed in a future branch, for now keeping the code more straightforward
    to read down the page is of more value for students, I think.
    
    Responsiveness of movement control feels pretty good to me, I'll plan on
    messing around with jump control in an upcoming commit.

--- source/entities/player/Player.hx
+++ source/entities/player/Player.hx
@@ -1,5 +1,6 @@
 package entities.player;
 
+import flixel.FlxObject;
 import flixel.FlxG;
 import flixel.FlxSprite;
 
@@ -12,19 +13,23 @@ class Player extends FlxSprite
     private static var WIDTH(default, never):Int = 32;
     private static var HEIGHT(default, never):Int = 64;
 
-    private static var MOVE_SPEED(default, never):Float = 250;
     private static var JUMP_SPEED(default, never):Float = -300;
     private static var GRAVITY(default, never):Float = 250;
-    private static var MAX_SPEED_X(default, never):Float = 200;
     private static var MAX_SPEED_Y(default, never):Float = 480;
+
+    private static var MAX_SPEED_X(default, never):Float = 200;
+    private static var MIN_SPEED_X(default, never):Float = 50;
+    private static var DEFAULT_SPEED_X(default, never):Float = 100;
 
     ////////////////////
     // INITIALIZATION //
     ////////////////////
-    public function new() {
+    public function new(startingDirection:Int = FlxObject.RIGHT) {
         super();
         makeGraphic(WIDTH, HEIGHT);
         configureSpeed();
+
+        facing = startingDirection;
     }
 
     /**
@@ -39,27 +44,58 @@ class Player extends FlxSprite
     // Update //
     ////////////
     override public function update(elapsed:Float) {
-        setMoveSpeed();
+        setMoveSpeed(getInputDirection());
         jump();
         // super.update() updates position, etc. so we typcially want to do it last.
         super.update(elapsed);
     }
 
     /**
-        Simple WASD/Arrow-key movement. 
-        Don't forget that negative values move up/left, while positive values move down/right.
+        Given a direction of input, determines and sets player speed.
+        @param inputDirection Integer representing direction of input currently held. Should have
+            value of FlxObject.LEFT, FlxObject.RIGHT, or FlxObject.NONE
     **/
-    private function setMoveSpeed() {
+    private function setMoveSpeed(inputDirection:Int) {
+        // Determine what speed player is moving at: max, default, or min
+        var speed:Float = 0;
+        if (inputDirection == facing) {
+            speed = MAX_SPEED_X;
+        } else if (inputDirection == FlxObject.NONE) {
+            speed = DEFAULT_SPEED_X;
+        } else { // Assumes that only other possible input direction is opposite of facing.
+            speed = MIN_SPEED_X;
+        }
+
+        // Determine what direction the player is moving in. Left is negative, right is positive.
+        var direction:Int = 0;
+        if (facing == FlxObject.LEFT) {
+            direction = -1;
+        } else { // Assumes player can only face left or right.
+            direction = 1;
+        }
+
+        velocity.x = speed * direction;
+    }
+
+    /**
+        Determines the direction of input, returns int representing the appropriate direction.
+        @see <http://api.haxeflixel.com/flixel/FlxObject.html> for info about directional values.
+    **/
+    private function getInputDirection():Int {
         var horizontalMovement:Int = 0;
         if (FlxG.keys.pressed.A || FlxG.keys.pressed.LEFT) {
             horizontalMovement--;
-        }
-
+        } 
+        
         if (FlxG.keys.pressed.D || FlxG.keys.pressed.RIGHT) {
             horizontalMovement++;
+        } 
+        
+        switch (horizontalMovement) {
+            case -1: return FlxObject.LEFT;
+            case 1: return FlxObject.RIGHT;
+            default: return FlxObject.NONE;
         }
-
-        velocity.x = horizontalMovement * MOVE_SPEED;
     }
 
     /**
